45229c94049efe1bc4e94474846aa61b1ba5bc9b
isnanramalia <isnanuramalia13@gmail.com>
2022-11-28 08:02:03 +0700

    edit daspro

--- C++/m2/daspro.cpp
+++ C++/m2/daspro.cpp
@@ -3,20 +3,42 @@ using namespace std;
 
 int main(){
     //nested loop - daspro tugas folio flowchart, step by step
-    int n;
+    int n=5;
 
     cout << "input baris: ";
     cin >> n;
 
     for(int i=1; i<=n; i++){
-        for(int j=1; j<i; j++){
-            for(int k=0; k<i; k++){
-                cout << i;
-            }
-            cout << "*";
+        for(int j=0; j<i; j++){
+            cout << i;
+        }
+        cout << " ";
+        for(int k=0; k<i; k++){
+            cout << i;
         }
         cout << endl;
     }
+
+    // for(int i=1; i<=n; i++){
+    //     for(int j=0; j<i; j++){
+    //         cout << i;
+    //     }
+    //     cout << i;
+    //     for(int k=0; k<i; k++){
+    //         cout << i;
+    //     }
+    //     cout << endl;
+    // }
+
+    // for(int i=1; i<=n; i++){
+    //     for(int j=1; j<i; j++){
+    //         for(int k=0; k<i; k++){
+    //             cout << i;
+    //         }
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
 
 
     return 0;
